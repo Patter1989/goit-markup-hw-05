f1d1a434e1f4e7092685d90343e185767ba988af
Patter1989 <125509804+Patter1989@users.noreply.github.com>
2023-04-09 13:00:04 +0200

    js remove modal

--- js/modal.js
+++ js/modal.js
@@ -5,8 +5,20 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn. addEventListener("click", toggleModal);
+  // refs.openModalBtn.foreach((element) => {
+  //   element.addEventListener("click", toggleModal);
+  // }); 
   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+
+
+  refs.modal.addEventListener('click', removeModal);
+  function removeModal(e) {
+    if (e.target === e.currentTarget) {
+      refs.modal.classList.add('is-hidden');
+    }
+  }
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
